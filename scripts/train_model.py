0431ce59435ad3cd52ae5c5573bac4f7c03f831d
Noturn1 <arthurangeloc@gmail.com>
2024-10-21 09:53:14 -0300

    Adicionar arquivos ao .gitignore e atualizar versões das dependências

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,72 +1,72 @@
 import os
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
 import json
-import matplotlib as plt
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+import matplotlib.pyplot as plt
 
-# Carrega as configs do projeto
+# Carregar as configs do projeto
 with open('/Users/arthurangelocencisilva/Programacao/detecta_tom_e_jerry/config.json') as config_file:
     config = json.load(config_file)
-    
-# Define as caminhos 
+
 train_data_dir = config["train_data_dir"]
 img_height, img_width = config["img_size"]
 batch_size = 32
 
-# Data augmentation
-train_datagen = ImageDataGenerator(rescale = 1./255, validation_split = 0.2)
+# Função para carregar imagens e rótulos
+def carregar_imagens_e_rotulos(diretorio):
+    imagens = []
+    rotulos = []
+    classes = os.listdir(diretorio)
+    class_indices = {classe: idx for idx, classe in enumerate(classes)}
+    for classe in classes:
+        caminho_classe = os.path.join(diretorio, classe)
+        if os.path.isdir(caminho_classe):
+            for nome_arquivo in os.listdir(caminho_classe):
+                caminho_imagem = os.path.join(caminho_classe, nome_arquivo)
+                if os.path.isfile(caminho_imagem):
+                    img = load_img(caminho_imagem, target_size=(img_height, img_width))
+                    img_array = img_to_array(img)
+                    imagens.append(img_array)
+                    rotulos.append(class_indices[classe])
+    return np.array(imagens), np.array(rotulos)
 
-train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size = (img_height, img_width),
-    batch_size = batch_size,
-    class_mode = 'categorical',
-    subset = 'training' # Usar para treinamento
-)
+# Carregar imagens e rótulos
+imagens, rotulos = carregar_imagens_e_rotulos(train_data_dir)
 
-validation_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size = (img_height, img_width),
-    batch_size = batch_size,
-    class_mode = 'categorical',
-    subset = 'validation' # Usar para validação
-)
+# Dividir os dados em conjuntos de treinamento e teste
+X_train, X_test, y_train, y_test = train_test_split(imagens, rotulos, test_size=0.2, random_state=42)
 
-# Verificar para garantir que as classes estejam corretas
-print(train_generator.class_indices)
+# Codificar os rótulos como categorias
+y_train = to_categorical(y_train, num_classes=len(np.unique(rotulos)))
+y_test = to_categorical(y_test, num_classes=len(np.unique(rotulos)))
 
-# Criação do modelo
+# Criar geradores de dados
+train_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow(X_train, y_train, batch_size=batch_size)
+validation_generator = test_datagen.flow(X_test, y_test, batch_size=batch_size)
+
+# Função para criar o modelo
 def criar_modelo():
-    model = Sequential()
-    
-    # Camada Convolucional 1
-    model.add(Conv2D (32,(3,3), activation = 'relu', input_shape = (img_height, img_width, 3 )))
-    model.add(MaxPooling2D (pool_size = (2,2)))
-    
-    # Camada Convolucional 2
-    model.add(Conv2D (64,(3,3), activation = 'relu', input_shape = (img_height, img_width, 3 )))
-    model.add(MaxPooling2D (pool_size = (2,2)))
-    
-    # Camada Convolucional 3
-    model.add(Conv2D (128,(3,3), activation = 'relu', input_shape = (img_height, img_width, 3 )))
-    model.add(MaxPooling2D (pool_size = (2,2)))
-    
-    # Camada de Flatten
-    model.add(Flatten())
-    
-    # Camada totalmente conectada
-    model.add(Dense(512, activation = 'relu'))
-    model.add(Dropout(0,5))
-    
-    # Camada de Saída (4 classes: nenhum, ambos, tom, jerry)
-    model.add(Dense(4, activation = 'softmax')) 
+    model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 3)),
+        MaxPooling2D((2, 2)),
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Conv2D(128, (3, 3), activation='relu'),
+        MaxPooling2D((2, 2)),
+        Flatten(),
+        Dense(512, activation='relu'),
+        Dense(len(np.unique(rotulos)), activation='softmax')  # Número de classes
+    ])
     
     # Compilando o modelo
-    model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-    
-    model.save('models/cnn_model.h5')
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     
     return model
 
@@ -74,12 +74,10 @@ def criar_modelo():
 modelo = criar_modelo()
 
 # Treinando o modelo
-history = modelo.fit (
+history = modelo.fit(
     train_generator,
-    epochs = 5,
-    steps_per_epoch = train_generator.samples // train_generator.batch_size,
-    validation_data = validation_generator,
-    validation_steps = validation_generator.samples // validation_generator.batch_size
+    epochs=5,
+    validation_data=validation_generator
 )
 
 # Salvando o histórico de treinamento
